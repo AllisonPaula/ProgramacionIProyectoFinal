236c7c4c3c79b83e97bd369d9a1098fafb3ea81f
AllisonPaula <105819821+AllisonPaula@users.noreply.github.com>
2023-08-10 18:49:03 -0400

    Add files via upload

---
+++ ProyectoProgramacionHotel/Base de Datos/Base de Datos.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE hotel_db;
+USE hotel_db;
+
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario VARCHAR(50) NOT NULL,
+    contrasena VARCHAR(50) NOT NULL,
+    tipo_usuario ENUM('Administrador', 'Corriente') NOT NULL
+);
+INSERT INTO usuarios (usuario, contrasena, tipo_usuario)
+VALUES
+    ('Juan', 'admin123', 'Administrador'),
+	('Jose', 'admin1234', 'Administrador'),
+    ('Ana', 'userpass1', 'Corriente'),
+    ('Elisa', 'userpass', 'Corriente');
+    
+    CREATE TABLE Huesped (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellido VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Telefono VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE Reserva (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    HuespedID INT NOT NULL,
+    CantidadAdultos INT NOT NULL,
+    CantidadNinos INT NOT NULL,
+    TipoHabitacion VARCHAR(50) NOT NULL,
+    PreferenciasEspeciales VARCHAR(100),
+    CheckIn DATE NOT NULL,
+    CheckOut DATE NOT NULL,
+    FOREIGN KEY (HuespedID) REFERENCES Huesped(ID)
+);
+
+CREATE TABLE Pago (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ReservaID INT NOT NULL,
+    NumeroTarjeta VARCHAR(25) NOT NULL,
+    MesVencimiento VARCHAR(10) NOT NULL,
+    AnioVencimiento VARCHAR(10) NOT NULL,
+    FOREIGN KEY (ReservaID) REFERENCES Reserva(ID)
+);
+SELECT *FROM Reserva;
---
+++ ProyectoProgramacionHotel/src/Reservacion/DatabaseConnection.java
@@ -0,0 +1,39 @@
+package Reservacion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+    private final String DATABASE_URL = "jdbc:mysql://localhost/hotel_db";
+    private final String DATABASE_USER = "root";
+    private final String DATABASE_PASSWORD = "Allison.031"; 
+
+    private DatabaseConnection() {
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized DatabaseConnection getInstance1() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+	public static Statement getInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
